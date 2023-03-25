0658d80f80ff973cfe08d35820f199fd712aa247
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2023-03-25 22:29:35 +0530

    JS and CSS fixed

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from .models import BigramLanguageModel,MultiHeadAttention,Head,device,stoi,decode
 # Create your views here.
 import os
@@ -12,13 +12,17 @@ pathToModel=os.path.join(os.getcwd(),'api','gpt1model','GPT5000.pb')
 m = torch.load(pathToModel,map_location=torch.device('cpu'))
 m.eval()
 print(pathToModel)
-@api_view(['GET'])
+@api_view(['POST'])
 def modelTest(request):
     # model = BigramLanguageModel()
     # print(os.getcwd())
     # print(device)
+    num_char=int(request.POST['num_char'])
     context = torch.zeros((1, 1), dtype=torch.long, device=device)
-    outputString=decode(m.generate(context, max_new_tokens=40)[0].tolist())
+    # context[0]
+    outputString=decode(m.generate(context, max_new_tokens=num_char)[0].tolist())
+    # outputString=outputString.replace("\n",'<br />')
     output={'outputString':outputString}
-    return Response(output)
+    print(output)
+    return JsonResponse(output)
 
